4cecfe83235d1a3f1a3945533f0c355a24884766
Stevi Hall <career.sh.work@gmail.com>
2020-02-03 18:43:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,20 +1,26 @@
 def create_an_empty_array
-  
+ create_an_empty_array = [] 
 end
 
 def create_an_array
-  
+  create_an_array = ["pineapple", "strawberries", "mangos", "icecream"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = ["method", "string"]
+  add_element_to_end_of_array.push(array, element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array,element)
+  add_element_to_start_of_array = ["array", "element"]
+  add_element_to_start_of_array("array").first
+ #puts add_element_to_start_of_array.inspect
+
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = ["method", "string", "array"]
+  remove_element_from_end_of_array = remove_element_from_end_of_array.pop(array)
   
 end
 
